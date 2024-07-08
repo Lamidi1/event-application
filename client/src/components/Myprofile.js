1cf2c21bba5d053ec21b6bb143780850ce71182c
lamidi1 <labassgbolahan@gmail.com>
2024-07-08 12:28:47 -0700

    myprofile

--- client/src/components/Myprofile.js
+++ client/src/components/Myprofile.js
@@ -4,6 +4,9 @@ import {  FaRegCircleUser } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 
 const Myprofile = () => {
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+  }
   return (
     <div className='p-6'>
      <h1 className=' font-semibold pl-7'>
@@ -19,6 +22,46 @@ const Myprofile = () => {
         Name
         <h2 className=' border-b-2 font-semibold'>Ayantola David</h2>
      </div>
+
+
+     <div className='mt-5'>
+        Date of birth
+        <h2 className=' border-b-2 font-semibold'>15/03/2001</h2>
+     </div>
+
+     <div className='mt-5'>
+        Gender
+        <h2 className=' border-b-2 font-semibold'>male</h2>
+     </div>
+
+
+     <div className='mt-5'>
+        Email
+        <h2 className=' border-b-2 font-semibold'>jayden@gmail.com</h2>
+     </div>
+
+
+     <div className='mt-5'>
+        Nationality
+        <h2 className=' border-b-2 font-semibold'>Nigeria</h2>
+     </div>
+
+
+     <div className='mt-5'>
+        Phone nuumber
+        <h2 className=' border-b-2 font-semibold'>07053456783</h2>
+     </div>
+
+    <form className='mt-4' onSubmit={handleSubmit}>
+    <button
+          type="submit"
+          className="p-2 w-full pl-10 border-2 border-slate-400 bg-blue-600  text-white rounded-md"
+        >
+          Edit profile
+        </button>
+
+    </form>
+
 
 
 
